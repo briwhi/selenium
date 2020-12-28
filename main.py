883556a9fbee1560de8253d5abaa8d6afa99eadb
Brian Whiteley <brianwhiteleybw@gmail.com>
2020-12-27 23:59:27 -0600

    python event scrape

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from selenium import webdriver
 
 chrome_driver_path = "C:/Users/brian/Documents/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-driver.get("https://www.amazon.com")
+driver.get("https://www.python.org")
+menu = driver.find_elements_by_xpath('//*[@id="content"]/div/section/div[3]/div[2]/div/ul/li')
+print(menu[1].text.split("\n"))
+events = {}
+x = 0
+for event in menu:
+    event_list = event.text.split("\n")
+    date = event_list[0]
+    name = event_list[1]
 
+    events[x] = {"time":date, "name":name}
+    x+=1
+print(events)
 driver.quit()
